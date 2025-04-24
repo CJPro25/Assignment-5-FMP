5ba4ee7ec410c2dbb559977e05f081228ccae409
Charlie Hale <147997731+CJPro25@users.noreply.github.com>
2025-04-24 16:14:56 +0100

    idk this was fyhren

---
+++ Unity/Assignment 5 - FMP/Assets/TestButton.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestButton : MonoBehaviour
+{    public void buttonPressed(string DebugMessage)
+    {
+        Debug.Log(DebugMessage);
+        // Find how to activate button
+    }
+}
