c5a0138525dd157bef3b4156c52c2c40aaad4354
Charlie Hale <147997731+CJPro25@users.noreply.github.com>
2025-06-02 13:01:08 +0100

    ALL FINSIHED

--- Unity/Assignment 5 - FMP/Assets/Quit.cs
+++ Unity/Assignment 5 - FMP/Assets/Quit.cs
@@ -1,3 +1,15 @@
 using UnityEngine;
-public class Quit : MonoBehaviour
-{public void QuitGame(){Application.Quit();UnityEditor.EditorApplication.isPlaying = false;}}
+
+public class QuitGame : MonoBehaviour
+{
+    public void quitTheGame()
+    {
+        // If we're in the Unity Editor, stop playing the scene
+#if UNITY_EDITOR
+        Debug.Log("Tried to quit");
+        UnityEditor.EditorApplication.isPlaying = false;  // This stops the play mode in the Unity editor
+#else
+            Application.Quit(); // Quit the game in a built version
+#endif
+    }
+}
